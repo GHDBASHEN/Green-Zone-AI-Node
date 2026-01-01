1341e8ea107de0cbd8b5f7a732dcd15f61de47bf
GHDBASHEN <ghdbashen@gmail.com>
2026-01-01 20:27:36 +0530

    Fix: Switch to Free Tier EC2 architecture

--- infra/main.tf
+++ infra/main.tf
@@ -4,28 +4,27 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "ghdb-terraform-state-bucket"
+    bucket = "ghdb-terraform-state-bucket" 
     key    = "green-zone/terraform.tfstate"
     region = "eu-north-1"
   }
 }
 
-# 1. Create a Single Server (Free Tier Eligible)
-resource "aws_instance" "green_zone_node" {
-  ami           = "ami-09a9858973b288bdd" # Ubuntu 24.04 in eu-north-1
-  instance_type = "t3.micro"              # Free Tier Eligible
 
-  # Open ports for App (8000), Grafana (3000), and SSH (22)
+resource "aws_instance" "green_zone_node" {
+  ami           = "ami-09a9858973b288bdd" # Ubuntu 24.04 (eu-north-1 )
+  instance_type = "t3.micro"              
+
+
   vpc_security_group_ids = [aws_security_group.allow_traffic.id]
   
-  # Install Docker & Start the App automatically
+
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y docker.io docker-compose
               
-              # Clone your repo (Public) or Pull Docker Image
-              # For simplicity, we just run the containers directly here:
+
               echo "
               services:
                 app:
@@ -40,6 +39,7 @@ resource "aws_instance" "green_zone_node" {
                   ports: ['3000:3000']
               " > docker-compose.yml
               
+              
               sudo docker-compose up -d
               EOF
 
@@ -48,7 +48,7 @@ resource "aws_instance" "green_zone_node" {
   }
 }
 
-# 2. Security Group (Firewall)
+
 resource "aws_security_group" "allow_traffic" {
   name        = "${var.client_name}-sg"
   description = "Allow inbound traffic"
@@ -57,19 +57,19 @@ resource "aws_security_group" "allow_traffic" {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # App Access
+    cidr_blocks = ["0.0.0.0/0"] 
   }
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Grafana Access
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH Access
+    cidr_blocks = ["0.0.0.0/0"] 
   }
   egress {
     from_port   = 0
@@ -78,6 +78,7 @@ resource "aws_security_group" "allow_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 
 output "server_ip" {
   value = "http://${aws_instance.green_zone_node.public_ip}:8000"
